439271ab6e539fc772a2826cb0e8372436f73e74
kasperkuo <kasper.kuo@gmail.com>
2016-06-22 16:12:12 -0700

    added navbar

---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,23 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Navbar = React.createClass({
+
+  render: function() {
+    return (
+      <div className="navbar-container">
+        <li className="logo">Sports</li>
+        <ul>
+          <li>Basketball</li>
+          <li>Football</li>
+          <li>Hockey</li>
+          <li>Soccer</li>
+          <li>Baseball</li>
+        </ul>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Navbar;
