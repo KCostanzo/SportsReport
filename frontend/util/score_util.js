3945d117cc979b83dd9a099ffe5f68e6f3282f80
kasperkuo <kasper.kuo@gmail.com>
2016-06-23 16:10:29 -0700

    added real time score update

--- frontend/util/score_util.js
+++ frontend/util/score_util.js
@@ -2,11 +2,21 @@ var ScoreServerActions = require('../actions/score_server_actions');
 
 var ScoreUtil = {
 	fetchAllScores: function () {
-		
+		var date = new Date();
+		var month = date.getMonth() + 1;
+		//+1 becasue getMonth() gives us the previous month
+		var year = date.getFullYear();
+		var day = date.getDate();
+		var digits = [1,2,3,4,5,6,7,8,9];
+		if (digits.indexOf(month) !== -1) {
+			month = '0' + month;
+		}
 
 		$.ajax({
 			method: 'GET',
-			url: 'http://gd2.mlb.com/components/game/mlb/year_2016/month_06/day_23/master_scoreboard.json',
+			url: 'http://gd2.mlb.com/components/game/mlb/year_' +
+						year + '/month_' + month +
+						'/day_' + day + '/master_scoreboard.json',
 			success: function (scores) {
 				ScoreServerActions.getScores(scores);
 			}
